fffe51b17a9430ff0efaf4945a0294108c94f3a5
Nesterenko-Artem <petr79petrov1979@gmail.com>
2022-10-09 22:08:05 +0300

    fix

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 
 export const Wrapper = styled('div')`
-  border: 1px solid #fff;
+  /* border: 1px solid #fff; */
   width: 75%;
   margin: 0 auto;
   display: flex;
